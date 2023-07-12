3eb58d2c7bc497d6f8acd8507b5bb2bf84cdaea3
Jose Chavarria <chavarriaj84@gmail.com>
2023-07-11 20:41:32 -0400

    v1.0.1

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  fileServices(): string {
+    return 'Hola mundo!';
+  }
+
+  fileServicePost() {
+    return 'Hello world!'
   }
 }
