975cd9d4f2e9040083fe7aab288a57e18115aeef
Jose Chavarria <chavarriaj84@gmail.com>
2023-07-11 20:43:26 -0400

    v1.0.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,13 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('producto')
+  @Get()
   getHello1(): string {
     return this.appService.fileServices();
     // Hola mundo!
   }
 
-  @Post('producto')
+  @Post()
   getHello2(): string {
     return this.appService.fileServicePost();
     // Hello world!
